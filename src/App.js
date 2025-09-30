ba7a5b3189a737fb50a64aa7ab3ca7b26925d2b9
dennisquiros1 <dennis16.2004@gmail.com>
2025-09-30 02:06:47 -0600

    Initial Interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/Logo_planifica_u.png'
+import logosa from './images/logo-ucr-sede-atlantico.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='body'>
+      <header className='App-header'>
+        <img className="logop" src={logo} alt="Logo Planifica U" />
       </header>
+
+      <div className='main-container'>
+        <div class="div-izquierdo">
+          <h1>PLANIFICA U</h1>
+          <h2>Sistemas de Planificación de Horarios para la Sede del Atlántico</h2>
+        </div>
+        <div class="div-derecho">
+          <h1>INICIAR SESIÓN</h1>
+          <div className='cuadro-usuario'>
+            <label>Usuario Institucional</label>
+            <input type='text'></input>
+          </div>
+          <div className='cuadro-contraseña'>
+            <label>Contraseña</label>
+            <input type='password'></input>
+          </div>
+          <div className='check'>
+            <input type='checkbox'></input>
+            <label>Recordar Usuario</label>
+          </div>
+          <div>
+            <button className='button'>Ingresar</button>
+          </div>
+        </div>
+      </div>
+
+      <footer className='App-footer'>
+        <img className='logosa' src={logosa} alt='Logo Sede Atlántico'/>
+      </footer>
     </div>
+    
   );
 }
 
